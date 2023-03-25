075e02061eadd4f856a2ad8fe4e9f53c339c8155
DaniilSV <daniilsivacenko@gmail.com>
2023-03-25 10:51:58 +0300

    ugh...

--- SupercellFlash/include/SupercellFlash/io/SWFStream.h
+++ SupercellFlash/include/SupercellFlash/io/SWFStream.h
@@ -33,7 +33,7 @@ namespace sc {
 			BufferStream input(&buffer);
 			WriteFileStream output(filepath);
 
-			if (signature != CompressionSignature::NONE) {
+			if (signature == CompressionSignature::NONE) {
 				output.write(buffer.data(), buffer.size());
 			}
 			else {
