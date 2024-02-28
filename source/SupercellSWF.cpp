4efac76a86d70730b1f1b349cfea864430f8bd78
Daniil-SV <daniilsivacenko@gmail.com>
2024-02-28 18:38:31 +0200

    Fixed saving for internal textures

--- source/SupercellSWF.cpp
+++ source/SupercellSWF.cpp
@@ -397,7 +397,7 @@ namespace sc
 
 			size_t position = stream.write_tag_header(texture.tag(*this, has_data));
 			texture.save(*this, has_data, is_lowres);
-			if (use_external_texture_files)
+			if (use_external_texture_files && has_data)
 			{
 				texture.encoding(SWFTexture::TextureEncoding::KhronosTexture);
 
