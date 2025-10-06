e8f399250a2a357524ad5594713184a7d914e4eb
Generic_Name <genericname1911.official@gmail.com>
2025-10-07 03:15:01 +0530

    (feat.) Linux and Mac Build Scripts

--- scripts/unix/generate_xcode_project.sh
+++ scripts/unix/generate_xcode_project.sh
@@ -1,1 +1,14 @@
-# TODO: Mac OS support
+#!/bin/bash
+
+# Set dirs
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+REPO_ROOT="${SCRIPT_DIR}/../.."
+BUILD_DIR="${REPO_ROOT}/build"
+
+# Remake build
+rm -rf "$BUILD_DIR"
+mkdir -p "$BUILD_DIR"
+cd "$BUILD_DIR"
+
+# Generate Xcode project using CMake
+cmake "$REPO_ROOT" -G "Xcode" -DSC_FLASH_BUILD_TOOLS=ON --fresh
